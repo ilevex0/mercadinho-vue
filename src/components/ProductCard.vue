5ef90ea2dd59f57e0ac5f3b4ab492809183ada55
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-12-04 16:23:29 -0300

    transform product cards in component and delete add to cart buttons

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <img
+      :src="require(`@/assets/products_images/${product.image}.png`)"
+      :alt="product.imageAlt"
+      class="product-image"
+      @click="addToCart(product, product.id)"
+    />
+    <div>
+      <div>
+        <p class="product-title" @click="addToCart(product, product.id)">
+          {{ product.name }}
+        </p>
+        <p class="product-description" @click="addToCart(product, product.id)">
+          {{ product.description }}
+        </p>
+      </div>
+
+      <p class="product-price">R$ {{ product.price.toFixed(2) }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        index: {
+            type: Number,
+        },
+        product: {
+            type: Object,
+        },
+    }
+};
+</script>
+
+<style scoped>
+.product-image {
+  width: 100%;
+  border-radius: 8px;
+  cursor: pointer;
+}
+.product-image:hover {
+  scale: 1.01;
+  transition: 0.5s;
+}
+.product-title,
+.product-description {
+  display: block;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
+  font-size: clamp(0.7rem, 0.8vw, 1.5rem);
+}
+.product-price {
+  font-weight: bold;
+  cursor: auto;
+  font-size: clamp(1rem, 1.2vw, 2rem);
+  white-space: nowrap;
+}
+</style>
--- src/components/ProductsComponent.vue
+++ src/components/ProductsComponent.vue
@@ -12,69 +12,20 @@
           v-for="(product, index) in filteredTechProducts"
           :key="index"
         >
-          <img
-            :src="require(`@/assets/products_images/${product.image}.png`)"
-            :alt="product.imageAlt"
-            class="product-image"
-            @click="addToCart(product, product.id)"
-          />
-          <div>
-            <p class="product-title" @click="addToCart(product, product.id)">
-              {{ product.name }}
-            </p>
-            <p
-              class="product-description"
-              @click="addToCart(product, product.id)"
-            >
-              {{ product.description }}
-            </p>
-
-            <div class="button-and-price">
-              <p class="product-price">R$ {{ product.price.toFixed(2) }}</p>
-              <b-button
-                class="btn-blue"
-                @click="addToCart(product, product.id)">{{ buttonText(product.id) }}</b-button
-              >
-            </div>
-          </div>
+          <ProductCard :product="product" :index="index" />
         </div>
       </div>
     </section>
     <section v-if="!failedToFetch">
       <h2 class="products-category">Best Sellers in Kitchen</h2>
       <div class="products">
-        <div
-          class="product"
-          v-for="(product, index) in filteredKichenProducts"
-          :key="index"
-        >
-          <img
-            :src="require(`@/assets/products_images/${product.image}.png`)"
-            :alt="product.imageAlt"
-            class="product-image"
-            @click="addToCart(product, product.id)"
-          />
-          <div>
-            <div>
-              <p class="product-title" @click="addToCart(product, product.id)">
-                {{ product.name }}
-              </p>
-              <p
-                class="product-description"
-                @click="addToCart(product, product.id)"
-              >
-                {{ product.description }}
-              </p>
-            </div>
-
-            <div class="button-and-price">
-              <p class="product-price">R$ {{ product.price.toFixed(2) }}</p>
-              <b-button
-                class="btn-blue"
-                @click="addToCart(product, product.id)"
-                >{{ buttonText(product.id) }}</b-button
-              >
-            </div>
+        <div class="products">
+          <div
+            class="product"
+            v-for="(product, index) in filteredKichenProducts"
+            :key="index"
+          >
+            <ProductCard :product="product" :index="index" />
           </div>
         </div>
       </div>
@@ -85,11 +36,13 @@
 <script>
 import CarouselComponent from "./CarouselComponent.vue";
 import axios from "axios";
+import ProductCard from "./ProductCard.vue";
 
 export default {
   name: "ProductsComponent",
   components: {
     CarouselComponent,
+    ProductCard,
   },
   data() {
     return {
@@ -170,72 +123,13 @@ section {
   align-content: center;
   justify-content: center;
 }
-.product-image {
-  width: 100%;
-  border-radius: 8px;
-  cursor: pointer;
-}
-.product-image:hover {
-  scale: 1.01;
-  transition: 0.5s;
-}
-.product-title,
-.product-description {
-  display: block;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  cursor: pointer;
-  font-size: clamp(0.7rem, 0.8vw, 1.5rem);
-}
 .products-category {
   font-size: clamp(1.2rem, 1.5vw, 6rem);
   font-weight: 600;
 }
-.product-price {
-  font-weight: bold;
-  cursor: auto;
-  font-size: clamp(1rem, 1.2vw, 2rem);
-  white-space: nowrap;
-}
-.button-and-price {
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
-  justify-items: flex-end;
-  flex-wrap: wrap;
-  gap: 0px 1rem;
-}
 .mb-2 {
   white-space: nowrap; /* Impede a quebra de linha */
   overflow: hidden; /* Esconde o texto que ultrapassar o limite */
-}
-
-.btn-blue {
-  width: 100%;
-  background-color: #007bff; /* Cor azul de fundo */
-  color: white; /* Cor do texto */
-  font-size: clamp(0.5rem, 1vw, 1rem); /* Tamanho da fonte */
-  padding: 10px; /* Espaçamento interno (padding) */
-  border: none; /* Remove a borda padrão */
-  border-radius: 8px; /* Bordas arredondadas */
-  cursor: pointer; /* Aparece a mãozinha ao passar o mouse */
-  transition: all 0.3s ease; /* Transição suave para os efeitos */
-  text-align: center; /* Centraliza o texto */
-  text-decoration: none; /* Remove o sublinhado caso seja link */
-  white-space: nowrap;
-}
-
-.btn-blue:hover {
-  background-color: #0056b3; /* Cor de fundo azul mais escura no hover */
-  transform: translateY(-2px); /* Pequeno efeito de movimento para cima */
-}
-
-.btn-blue:active {
-  background-color: #004085; /* Cor ainda mais escura no clique */
-  transform: translateY(2px); /* Efeito de pressionar o botão */
 }
 @media (max-width: 843px) {
   section {
